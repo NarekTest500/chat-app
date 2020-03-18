0831160fe005b444e30020e70ceb49aaffaa2853
NarekTest500 <imogtanun@gmail.com>
2020-03-18 17:34:09 +0400

    second-commit

---
+++ app/Http/Controllers/ChatsController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Events\MessageSent;
+use App\Message;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class ChatsController extends Controller
+{
+
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    public function index ()
+    {
+        return view('chats');
+    }
+
+    public function fetchMessages ()
+    {
+        return Message::with('user')->get();
+    }
+
+    public function sendMessages (Request $request)
+    {
+        $message = Auth::user()->messages()->create([
+            'message' => $request->message
+        ]);
+
+        broadcast(new MessageSent($message->load('user')))->toOthers();
+
+        return ['status' => 'Success'];
+    }
+
+}
